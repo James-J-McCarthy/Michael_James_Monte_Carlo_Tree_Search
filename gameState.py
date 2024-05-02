dfe5c9793e1348195cdfb9e831e67f2340835c43
James-J-McCarthy <112642209+James-J-McCarthy@users.noreply.github.com>
2024-05-02 18:20:33 -0500

    commit one win check working

--- gameState.py
+++ gameState.py
@@ -1,13 +1,17 @@
 
 class gameState:
-    def __init__(self, bitCode, p1Mask, turnNumber) -> None:
-        self.bitCode = bitCode
-        self.mask = p1Mask
-        self.turnNumber = turnNumber
+    def __init__(self, bitCode, p1Mask, isP1) -> None:
+        self.bitCode = int(bitCode)
+        self.mask = int(p1Mask)
+        self.playerTurn = isP1
 
-    def printToBinary(self):
-        print(bin(self.bitCode))
+    def printToBinary(self, bits):
+        print(bin(bits))
 
+    def rightShift(self, bits, n):
+        bits = int(bits / 2 ** n)
+        return bits
+    
     # call like: list = placeChar(list, "x", 2)
     def placeCharInLine(self, stringy, character, index):
         stringList = list(stringy)
@@ -33,8 +37,28 @@ class gameState:
                     # print("i: ", i, " j: ", j)
                     line = self.placeCharInLine(line, peice, (j*4)+2)
             print(line, "\n")
-
-            
+    
+    
+    def horizontalWin(self, winnerPeices):
+        self.printToBinary(winnerPeices)
+        self.printToBinary(self.rightShift(winnerPeices, 4))
+        self.printToBinary(self.rightShift(winnerPeices, 8))
+        return 0 < (winnerPeices & (self.rightShift(winnerPeices, 4)) & self.rightShift(winnerPeices, 8))
+        
+    def evaluate(self):
+        winnerBits = None
+        if(self.playerTurn):
+            winnerBits = self.bitCode & self.mask
+        else:
+            winnerBits = self.bitCode & (~self.mask)
+        print("entered bits: ")
+        self.printToBinary(winnerBits)
+        val = -1
+        val = self.horizontalWin(winnerBits)
+        if val == -1:
+            print("eval errro")
+        print("val", val)
+        return val
     
 
 
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import gameState
 
-gs = gameState.gameState(0b00101100110, 0b00001100100, 3)
+gs = gameState.gameState(0b00101100110, 0b00001100100, True)
+# gs.printBoard()
+# gs.evaluate()
+gs = gameState.gameState(0b10101010101, 0b10001010100, True)
 gs.printBoard()
+gs.evaluate()
+
+print("1100 & 100: ", bin(int(0b1000 / 2**1)))
 
